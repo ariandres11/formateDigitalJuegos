30fdd3161f2d3ef06b138986da1dda2cba1d02da
arielandres11 <arielsebastianandres11@gmail.com>
2024-05-17 09:13:26 -0300

    Ir y volver del juego a pantalla debug listo

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) => MaterialApp(
+    //cambiar por Menu() cuando menu sea Stateless
     home: DebugScreen(),
   );
 }
--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app/screens/debug_screen.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
@@ -5,10 +6,27 @@ import '../trenes_game.dart';
 
 class JuegoScreen extends StatelessWidget {
   final TrenesGame game;
+
   JuegoScreen(this.game);
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(body:
+      Column(
+        children: <Widget>[
+          ElevatedButton(onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DebugScreen())
+            );
+          },
+            // Reemplazar por Volver al menu
+        child: Text('Volver a debug screen'),
+          ),
+          GameWidget(game: game)
+        ],
+      )
+      ,);
     return GameWidget(game: game);
   }
 }
