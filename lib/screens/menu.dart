7c99303db39c9951cefe6c20f4db67b9d4d1fd6d
LuciaR22 <luciajazminrivero@gmail.com>
2024-05-18 01:37:26 -0300

    menu en proceso

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,55 +1,46 @@
-import 'package:app/components/button.dart';
-import 'package:app/trenes_game.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-import 'package:flutter/material.dart';
-import 'game_screen.dart';
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => Menu();
-}
-
-class Menu extends State<HomePage> {
-  List niveles = [
-    [
-      "1",
-      30,
-      12,
-      [2, 3]
-    ]
-  ];
+class Menu extends StatelessWidget {
+  const Menu({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          //itemCount: niveles.length,
-          itemBuilder: (context, index) {
-            return ListView(
-              children: <Widget>[
-                ElevatedButton(
-                    child: Text('Jugar'),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => JuegoScreen(TrenesGame())),
-                      );
-                    }),
-              ],
-            );
-          }),
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/fondo.png',
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          alignment: Alignment.center,
+          child: const Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: Text('Trenes', style: TextStyle(fontSize: 52),),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  child: const Text('juego'),
+                  onPressed: () { Navigator.pushNamed(context, '/menu');},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  child: const Text('Salir'),
+                  onPressed:() { Navigator.pushNamed(context, '/menu');},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
-/*            return Button(
-                text: "nivel " + niveles[index][0],
-                carbon: niveles[index][1],
-                vagones: niveles[index][2],
-                slots: niveles[index][3]
-            );*/
