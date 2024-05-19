067e2dd9b651bae75802d4410f147f79004c7e55
LuciaR22 <luciajazminrivero@gmail.com>
2024-05-19 20:35:19 -0300

    objetos juego resposivos

--- lib/trenes_game.dart
+++ lib/trenes_game.dart
@@ -1,40 +1,40 @@
 import 'dart:async';
-import 'package:app/cabecera_tren.dart';
 import 'package:app/components/bolsa.dart';
 import 'package:app/components/carbon.dart';
 import 'package:app/components/tolva.dart';
 import 'package:app/components/vagon.dart';
-import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 
 class TrenesGame extends FlameGame{
   TrenesGame(this.cantidadCarbon);
 
   final int cantidadCarbon;
+
   final  volverOverlay = 'Volver';
   final numberOverlay = 'NumberSelector';
 
   @override
   Future<void> onLoad() async{
-
+    final screenWidth = size.x;
+    final screenHigth = size.y;
     addAll([
       //Tolvas
-      Tolva(tolvaPosition: Vector2(620,240)),
-      Tolva(tolvaPosition: Vector2(780,240)),
+      Tolva(tolvaPosition: Vector2((screenWidth/2) - 185, (screenHigth / 2) - 120)),
+      Tolva(tolvaPosition: Vector2((screenWidth/2) + 95,(screenHigth / 2) - 120)),
       //Bolsa
-      Bolsa( bolsaPosition: Vector2(700,40)),
+      Bolsa( bolsaPosition: Vector2((screenWidth/2) - 20, 40)),
       //Vagones
-      Vagon(cantidadSlots:3, vagonPosition: Vector2(620,500)),
-      Vagon(cantidadSlots: 2, vagonPosition: Vector2(790,500)),
+      Vagon(cantidadSlots:3, vagonPosition: Vector2((screenWidth/2) - 175, screenHigth - 200)),
+      Vagon(cantidadSlots: 2, vagonPosition: Vector2((screenWidth/2) + 105 ,screenHigth - 200)),
 
     ]);
     bool tolvapar = true;
     for(int i = 0; i < cantidadCarbon; i++) {
       tolvapar = !tolvapar;
       if (tolvapar) {
-        add(Carbon(carbonPosition: Vector2(780, 240 + i * 10.0)));
+        add(Carbon(carbonPosition: Vector2((screenWidth/2) - 130, 240 + i * 10.0)));
       } else {
-        add(Carbon(carbonPosition: Vector2(620, 240 + i * 10.0)));
+        add(Carbon(carbonPosition: Vector2((screenWidth/2) + 150, 240 + i * 10.0)));
       }
     }
     overlays.add(volverOverlay);
